1b147914964348f24a68e7853e6c8c2acfd921a9
Maksim Dragun <maksimdragun@live.com>
2021-03-31 03:01:32 +0300

    Cuprumberry blog

--- cuprumberry/cuprumberry/core/urls.py
+++ cuprumberry/cuprumberry/core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('', RedirectView.as_view(url='index')),
     path('catalog', views.catalog, name='catalog'),
+    path('catalog/categories/<int:category_id>/<str:category_name>', views.catalog_category, name='catalog_category'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('products/<int:product_id>/order', views.quick_order, name='quick_order'),
 
--- cuprumberry/cuprumberry/core/views.py
+++ cuprumberry/cuprumberry/core/views.py
@@ -1,9 +1,25 @@
+from enum import Enum
+from typing import Optional
+
+from django.db.models import Case, When, Value
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
 
 from .models import Product, ProductOrder, ProductImage
+
+
+class DisplayEnum(Enum):
+    TILES = 'table'
+    LIST = 'list'
+
+
+class SortEnum(Enum):
+    DATE_DESC = '-created_datetime'
+    DATE_ASC = 'created_datetime'
+    PRICE_DESC = '-real_price'
+    PRICE_ASC = 'real_price'
 
 
 def index(request):
@@ -15,6 +31,10 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
+def catalog_category(request, category_id: Optional[int], category_name: Optional[str]):
+    return catalog(request)
+
+
 def catalog(request):
     display: str = request.GET.get('display')
     if 'display' not in request.session:
@@ -22,11 +42,30 @@ def catalog(request):
     elif display is not None:
         request.session['display'] = display
 
-    products: [Product] = Product.objects.order_by('-created_datetime')[:5]
+    try:
+        sort: SortEnum = SortEnum[request.GET.get('sort')]
+    except KeyError:
+        sort: SortEnum = SortEnum.DATE_DESC
+    request.session['sort'] = sort.value
+
+    page: int = int(request.GET.get('page', '1'))
+    page_size: int = 2
+    total: int = Product.objects.count()
+    total_pages: int = (total - 1) // page_size + 1
+    products: [Product] = Product.objects.annotate(
+        real_price=Case(
+            When(actual_price__isnull=False, then='actual_price'),
+            default='price',
+        )
+    ).order_by(sort.value)[:5]
     context = {
+        'sorts': {sort.name: sort.value for sort in SortEnum},
         'products': {
             product: ProductImage.objects.get(product=product, order=0) for product in products
         },
+        'page': page,
+        'total_pages': total_pages,
+        'pages': range(1, total_pages + 1)
     }
     if request.is_ajax():
         return render(request, 'catalog/product-list.html', context)
